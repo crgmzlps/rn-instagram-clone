9457f9b3f99a4552ae82e281a45cffb38b3d4d4e
Carlo <crgmzlps@gmail.com>
2023-09-26 13:20:53 -0300

    Post component UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,137 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import colors from './theme/colors';
 import fonts from './theme/fonts';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
 
 function App(): JSX.Element {
   return (
-    <View style={styles.root}>
-      <Text style={styles.text}>
-        Hello React Native <AntDesign name="stepforward" size={18} />
-      </Text>
+    <View style={styles.post}>
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>username</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+        }}
+        style={styles.image}
+      />
+      <View style={styles.footer}>
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name="hearto"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+        {/* Likes */}
+        <Text style={styles.text}>
+          Liked by <Text style={styles.bold}>dummy45</Text> and{' '}
+          <Text style={styles.bold}>66 others</Text>
+        </Text>
+        {/* Post description */}
+        <Text style={styles.text}>
+          <Text style={styles.bold}>username</Text> Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit.
+        </Text>
+        {/* Comments */}
+        <Text>View all 16 comments</Text>
+        <View style={styles.comment}>
+          <Text style={styles.commentText}>
+            <Text style={styles.bold}>dummy45</Text> Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit.
+          </Text>
+          <AntDesign name="hearto" style={styles.icon} color={colors.black} />
+        </View>
+        {/* Posted Date */}
+        <Text>20 December, 2022</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: colors.lightgrey,
+  post: {},
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    padding: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: fonts.weight.bold,
+    color: colors.black,
+  },
+  threeDots: {
+    marginLeft: 'auto',
+  },
+  footer: {
+    padding: 10,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  icon: {
+    marginHorizontal: 5,
   },
   text: {
-    fontSize: fonts.size.lg,
-    color: colors.white,
-    backgroundColor: colors.grey,
+    color: colors.black,
+    lineHeight: 18,
+  },
+  bold: {
+    fontWeight: fonts.weight.bold,
+  },
+  comment: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  commentText: {
+    color: colors.black,
+    flex: 1,
+    lineHeight: 18,
   },
 });
 
